415432987d0e043e641207c82ac8d9678dba97f0
Facundo <rutzivdayne@gmail.com>
2023-05-14 15:55:29 +0800

    Fourth Commit, successfully added ViewPrevious.java and hooked it up to the associated button

--- app/src/main/java/com/example/financemanager/MainActivity.java
+++ app/src/main/java/com/example/financemanager/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.financemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button ViewPreviousButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewPreviousButton = (Button)findViewById(R.id.ViewPreviousButton);
+    }
+
+    public void ViewPrevious(View view){
+        Intent intent = new Intent(this, ViewPrevious.class);
+        startActivity(intent);
+        Toast.makeText(getApplicationContext(),"Now Viewing Previous Revenues and Expenses", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/financemanager/ViewPrevious.java
@@ -0,0 +1,14 @@
+package com.example.financemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewPrevious extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_previous);
+    }
+}
